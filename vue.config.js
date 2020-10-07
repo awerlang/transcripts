c81ad2bdb74434172f8aab2ebdbb0a4551546ea3
André Werlang <beppe85@gmail.com>
2020-10-07 15:40:30 -0300

    feat: change app title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'Transcript Analyzer'
+                return args
+            })
+    },
     devServer: {
         proxy: 'http://localhost:3000',
     },
